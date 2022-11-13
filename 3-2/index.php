3aee4cbba26b1b25c2cd78f3dc8a12abf72b160a
19841985ykram <116469002+19841985ykram@users.noreply.github.com>
2022-11-13 10:09:33 +0900

    3章　PHP初級　練習問題1
    index.phpの作成

---
+++ 3-2/index.php
@@ -0,0 +1,24 @@
+<?php
+  //商品の税込価格を計算しましょう
+  //①税率を定数TAXで作成しましょう。消費税は10%とします。
+  //定数の宣言、定数（TAX)
+  define("TAX",0.1);
+  //②商品の情報を連想配列に入れましょう。
+    $products = ["鉛筆" => 100, "消しゴム" => 150, "物差し" => 500];
+
+  //③税率を計算する関数を用意します。
+  //引数には値段を受け取り、税込価格を返答します。
+  function tax_price($price) {
+    $taxprice = $price + $price * TAX;
+    echo $taxprice;
+  };
+  //関数を呼び出す
+  //④繰り返し文を使って画面に指定の文字を表示しましょう！
+  foreach ($products as $key => $value) {
+    echo $key;
+    echo "の税込価格は";
+    tax_price($value);
+    echo "円です";
+    echo '<br>';
+  };
+  ?>
