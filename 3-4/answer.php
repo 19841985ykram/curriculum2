cd73c0cb4084e9c179687d196549302d44ae6c7b
19841985ykram <116469002+19841985ykram@users.noreply.github.com>
2022-11-25 11:09:20 +0900

    PHP初級チェックリスト
    提出用１回目です。

--- 3-4/answer.php
+++ 3-4/answer.php
@@ -1,34 +1,42 @@
+<link rel="stylesheet" href="style.css">
 <?php
-//[question.php]から送られてきた名前の変数、選択した回答、問題の答えの変数を作成
+//[question.php]から送られてきた名前の変数
 $my_name = $_POST['my_name'];
-// 🟡選択した回答の変数はこれであっているのか？
+// 選択した回答の変数
 $q1 = $_POST["q1"];
 $q2 = $_POST["q2"];
 $q3 = $_POST["q3"];
-// 🟡問題の答え変数はこれであっているのか？
-$q1_a = $_POST["80"];
-$q2_a = $_POST["HTML"];
-$q3_a = $_POST["select"];
-
+// 問題の答えの変数
+$q1_a = $_POST["q1_a"];
+$q2_a = $_POST["q2_a"];
+$q3_a = $_POST["q3_a"];
 //選択した回答と正解が一致していれば「正解！」、一致していなければ「残念・・・」と出力される処理を組んだ関数を作成する
-// 🟡引数は（①選択した回答の変数、②正解の変数）だと思うが、({引数①$q1,$q2,$q3},{引数②$q1_a,$q2_a,$q3_a})は明らかにおかしいと感じるが、どう定義したら良いかが分からない？
-function question () {
-  if( == ) {
+function question ($select,$answer) {
+  if($select == $answer) {
     $result = "正解！";
+    //返り値を取得する
+    return $result;
 } else {
     $result = "残念・・・";
+    return $result;
 }
 }
-question(); //🟡引数の値は何を入れたら良いかがテキストを見ても分からない。
+//関数の呼び出し
+$result = question($q1,$q1_a);
+$result = question($q2,$q2_a);
+$result = question($q3,$q3_a);
 ?>
 
 <p><?php echo $my_name; ?>さんの結果は・・・？</p>
 <p>①の答え</p>
 <!--作成した関数を呼び出して結果を表示-->
-<?php echo $result; ?>
+<p><?php $result = question($q1,$q1_a);?></p>
+<p><?php echo $result;  ?></p>
 <p>②の答え</p>
 <!--作成した関数を呼び出して結果を表示-->
-<?php echo $result; ?>
+<p><?php $result = question($q2,$q2_a);?></p>
+<p><?php echo $result; ?></p>
 <p>③の答え</p>
 <!--作成した関数を呼び出して結果を表示-->
-<?php echo $result; ?>
+<p><?php $result = question($q3,$q3_a);?></p>
+<p><?php echo $result; ?></p>
--- 3-4/question.php
+++ 3-4/question.php
@@ -1,3 +1,4 @@
+<link rel="stylesheet" href="style.css">
 <?php
 //POST送信で送られてきた名前を受け取って変数を作成
 $my_name = $_POST['my_name'];
@@ -7,8 +8,6 @@ $q1 = ["80","22","20","21"];
 $q2 = ["PHP","Python","JAVA","HTML"];
 $q3 = ["join","select","insert","update"];
 //② ①で作成した、配列から正解の選択肢の変数を作成してください
-// 🟡「正解の選択肢の変数」を作成するという目的が分からない？
-// 🟡下記の変数であっているのか？ 配列ではダメなのか？ $answer = ["80","HTML","select"];
 $q1_a = "80";
 $q2_a = "HTML";
 $q3_a = "select";
@@ -20,23 +19,23 @@ $q3_a = "select";
 <h2>①ネットワークのポート番号は何番？</h2>
 <!--③ 問題のradioボタンを「foreach」を使って作成する-->
 <?php foreach($q1 as $value) { ?>
-    <input type="radio" name="q1" value="<?php echo $value; ?>" /> <?php echo $value; ?><br>
+    <input type="radio" name="q1" value="<?php echo $value; ?>" /> <?php echo $value; ?>
 <?php } ?>
 <h2>②Webページを作成するための言語は？</h2>
 <!--③ 問題のradioボタンを「foreach」を使って作成する-->
 <?php foreach($q2 as $value) { ?>
-    <input type="radio" name="q2" value="<?php echo $value; ?>" /> <?php echo $value; ?><br>
+    <input type="radio" name="q2" value="<?php echo $value; ?>" /> <?php echo $value; ?>
 <?php } ?>
 <h2>③MySQLで情報を取得するためのコマンドは？</h2>
 <!--③ 問題のradioボタンを「foreach」を使って作成する-->
 <?php foreach($q3 as $value) { ?>
-    <input type="radio" name="q3" value="<?php echo $value; ?>" /> <?php echo $value; ?><br>
-<?php } ?>
-<!--問題の正解の変数と名前の変数を[answer.php]に送る-->
-<input type="hidden" name="my_name" value="<?php echo $my_name;?>"/>
-<!-- 🟡「問題の正解の変数」を送るという処理は下記の記述であっているのか？ -->
-<input type="hidden" name="80" value="<?php echo $q1_a;?>"/>
-<input type="hidden" name="HTML" value="<?php echo $q2_a;?>"/>
-<input type="hidden" name="select" value="<?php echo $q3_a;?>"/>
-<input type="submit" value="回答する" />
+    <input type="radio" name="q3" value="<?php echo $value; ?>" /> <?php echo $value; ?>
+<?php } ?><br>
+<!-- 名前の変数を[answer.php]に送る -->
+<input type="hidden" name="my_name" value="<?php echo $my_name;?>"/><br>
+<!-- 問題の正解の変数を[answer.php]に送る -->
+<input type="hidden" name="q1_a" value="<?php echo $q1_a;?>"/>
+<input type="hidden" name="q2_a" value="<?php echo $q2_a;?>"/>
+<input type="hidden" name="q3_a" value="<?php echo $q3_a;?>"/>
+<input type="submit" value="回答する"/>
 </form>
