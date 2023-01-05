2b1abc8e69f2c44cbdceda4112a649091c9935c1
19841985ykram <116469002+19841985ykram@users.noreply.github.com>
2023-01-05 18:02:11 +0900

    PHP4章中級　バグ修正を行う
    test1は完了しました。
    test2について、質問させていただきたいです。

---
+++ 4-1/bugTest/test2/dbconnect.php
@@ -0,0 +1,13 @@
+<?php
+// セッション開始
+// 🟡session_start();
+// DBサーバのURL
+$db['host'] = "localhost";
+// ユーザー名
+$db['user'] = "root";
+// ユーザー名のパスワード
+// 🟡$db['pass'] = "";
+$db['pass'] = "root";
+// データベース名
+// 🟡$db['dbname'] = "loginManagement";
+$db['dbname'] = "YIGroupBlog";
